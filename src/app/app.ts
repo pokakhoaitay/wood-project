03b4fabd8ae73e8dd55fd4d9c55469bcbf00f6a1
tron.nguyen <pokakhoaitay@live.com>
2015-12-05 01:32:57 +0700

    init project ok

---
+++ src/app/app.ts
@@ -0,0 +1,20 @@
+
+
+//noinspection TypeScriptCheckImport
+import{Component, View, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+    selector: 'my-app',
+    template:'<h1>I love Angular2</h1>'
+})
+
+export class AppCmp{
+
+}
+
+//noinspection TypeScriptValidateTypes
+bootstrap(AppCmp,[
+
+])
+
---
+++ typings/my-type.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by Poka on 12/5/2015.
+ */
+
+declare class AppCmp{}
